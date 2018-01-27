5c52d9a72a9d93424d47e39da85e1c87ab7e6016
Michael Eggers <mdeggers84@gmail.com>
2018-01-27 14:15:31 -0500

    state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />;
-  }
+  constructor(props) {
+    super(props);
 
-  onInputChange(event) {
-    console.log(event.target.value);
+    this.state = { term: '' };
+  }
+  render() {
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 }
 
