4c0a28b16223afab19ab1e9bfde6f79f8106942b
Michael Eggers <michael@meggers.me>
2018-02-28 06:56:57 -0500

    callback and style

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyCWeGbxWxkHx8k95fVuwDJKf7ijpRQ0Lcs';
-// Create a new component.
-// This component should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+const API_KEY = 'AIzaSyDwOj0KRYj0lRLgGocauIWP8f3cOuSI37k';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      }); // same as this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 // Take this component's generated HTML and put it
 // on the page ( in the DOM )
